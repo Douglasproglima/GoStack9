14b6aa121393618ea076613a0245b3b833a077bd
Douglas Lima <douglasproglima@gmail.com>
2020-06-23 21:16:41 -0300

    Routes, Route Params e Query Params

--- modulo_1/index.js
+++ modulo_1/index.js
@@ -2,8 +2,17 @@ const express = require('express');
 const server = express();
 const port = 3000;
 
-server.get('/', (requestBody, response) => {
-  return response.json({ message: 'Hello World'});
+//http://localhost:3000/login/?name=Douglas
+server.get('/login', (request, response) => {
+  const name = request.query.name;
+  return response.json({ message: `Rota login: Hello World ${name}`});
+});
+
+//http://localhost:3000/users/1
+server.get('/users/:id', (req, res) => {
+  //const id = req.params.id;
+  const { id } = req.params;
+  return res.json({ message: `Rota: users id: ${id}` });
 });
 
 server.listen(port, () => {
