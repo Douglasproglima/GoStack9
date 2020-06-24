fc0ad7d0a2249403a36d72c80bf93616757770a2
Douglas Lima <douglasproglima@gmail.com>
2020-06-23 21:26:48 -0300

    Recebendo parametros nas rotas e retornando json

--- modulo_1/index.js
+++ modulo_1/index.js
@@ -9,10 +9,16 @@ server.get('/login', (request, response) => {
 });
 
 //http://localhost:3000/users/1
-server.get('/users/:id', (req, res) => {
+server.get('/user/:id', (req, res) => {
   //const id = req.params.id;
   const { id } = req.params;
   return res.json({ message: `Rota: users id: ${id}` });
+});
+
+const users = ['Douglas Lima', 'Ana Clara', 'Débora Leite'];
+server.get('/users/:index', (req, res) => {
+  const { index } = req.params;
+  return res.json(users[index]);
 });
 
 server.listen(port, () => {
