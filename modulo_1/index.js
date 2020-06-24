ae74765d2c5c57330159ec53747684d44925870e
Douglas Lima <douglasproglima@gmail.com>
2020-06-23 22:45:31 -0300

    CRUD - EXEMPLO

--- modulo_1/index.js
+++ modulo_1/index.js
@@ -2,23 +2,47 @@ const express = require('express');
 const server = express();
 const port = 3000;
 
+//Informa para o express que as requisições serão trafegadas usando JSON
+server.use(express.json());
+
+const users = ['Douglas Lima', 'João Exemplo', 'Débora Leite'];
+
 //http://localhost:3000/login/?name=Douglas
 server.get('/login', (request, response) => {
   const name = request.query.name;
   return response.json({ message: `Rota login: Hello World ${name}`});
 });
 
-//http://localhost:3000/users/1
-server.get('/user/:id', (req, res) => {
-  //const id = req.params.id;
-  const { id } = req.params;
-  return res.json({ message: `Rota: users id: ${id}` });
+//http://localhost:3000/users/2 => index do array users
+server.get('/users', (req, res) => {
+  return res.json(users);
 });
 
-const users = ['Douglas Lima', 'Ana Clara', 'Débora Leite'];
-server.get('/users/:index', (req, res) => {
-  const { index } = req.params;
-  return res.json(users[index]);
+//http://localhost:3000/users/1
+server.get('/users/:id', (req, res) => {
+  //const id = req.params.id;
+  const { id } = req.params;
+  return res.json(users[id]);
+});
+
+//http://localhost:3000/users => body: { "name": "nome_usuario" }
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+  users.push(name);
+  return res.json(users);
+});
+
+server.put('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  users[id] = name;
+  res.json(users);
+});
+
+server.delete('/users/:id', (req, res) => {
+  const { id } = req.params;
+  users.pop(id);
+  return res.json(users);
 });
 
 server.listen(port, () => {
