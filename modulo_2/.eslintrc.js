99fe6a826ae8a912e0f77e7a91514e22c53d9e6a
Douglas Lima <douglasproglima@gmail.com>
2020-06-28 21:26:17 -0300

    Sequelize/Migrations e Create Tables on DB

--- modulo_2/.eslintrc.js
+++ modulo_2/.eslintrc.js
@@ -14,9 +14,7 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    'prettier/prettier': ['error' {
-      "endOfLine":"auto",
-    }],
+    'prettier/prettier': 'error',
     'class-methods-use-this': 'off',
     'no-param-reassign': 'off',
     camelcase: 'off',
