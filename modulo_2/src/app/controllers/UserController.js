439514529a2c4c3ff4b3bf10879d3f793aa359ec
Douglas Lima <douglasproglima@gmail.com>
2020-07-01 23:10:30 -0300

    Validação de Campos do Body Requisição

--- modulo_2/src/app/controllers/UserController.js
+++ modulo_2/src/app/controllers/UserController.js
@@ -1,8 +1,20 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
     try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required(),
+        email: Yup.string().required(),
+        password: Yup.string().required().min(6),
+      });
+
+      if (!(await schema.isValid(req.body)))
+        return res
+          .status(400)
+          .json({ message: 'Falha na validação dos campos' });
+
       const userExists = await User.findOne({
         where: { email: req.body.email },
       });
@@ -55,6 +67,20 @@ class UserController {
 
   async update(req, res) {
     try {
+      const schema = Yup.object().shape({
+        name: Yup.string(),
+        email: Yup.string(),
+        old_password: Yup.string().required().min(6),
+        password: Yup.string()
+          .min(6)
+          .when('old_password', (oldPassword, field) =>
+            oldPassword ? field.required() : field
+          ),
+        confirmPassword: Yup.string().when('password', (password, field) =>
+          password ? field.required().oneOf([Yup.ref('password')]) : field
+        ),
+      });
+
       const { email, oldPassword } = req.body;
       const user = await User.findByPk(req.userId);
       if (!user) {
