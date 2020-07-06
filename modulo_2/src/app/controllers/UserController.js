4a5c251049102ed854cb8db44a45b050c6f45196
Douglas Lima <douglasproglima@gmail.com>
2020-07-05 22:02:13 -0300

    Listagem dos Agendamentos

--- modulo_2/src/app/controllers/UserController.js
+++ modulo_2/src/app/controllers/UserController.js
@@ -43,7 +43,7 @@ class UserController {
           {
             model: File,
             as: 'Avatar',
-            attributes: ['id', 'name', 'path'],
+            attributes: ['id', 'path', 'url'],
             order: [['id', 'DESC']],
           },
         ],
