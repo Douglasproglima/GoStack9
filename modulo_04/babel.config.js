ff771f4ca568b8f1de54022d34b078a329153fae
Douglas Lima <douglasproglima@gmail.com>
2020-07-19 19:41:52 -0300

    Start Módulo React

---
+++ modulo_04/babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  presets: [
+    "@babel/preset-env", //Responsavel por traduzir as funcionalidades do JS que o browser não entende
+    "@babel/preset-react" //Responsavel por traduzir as funcionalidades do react que o browser não entende
+  ],
+};
---
+++ modulo_04/src/index.js
@@ -0,0 +1,2 @@
+const helloWord = (a, b) => a + b;
+alert(helloWord(5 + 10));
