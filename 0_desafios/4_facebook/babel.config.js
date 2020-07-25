fba12f312914a9e9ace13c85673e59dca54c4c6a
Douglas Lima <douglasproglima@gmail.com>
2020-07-25 18:54:07 -0300

    Desafio 04 - React Facebook

---
+++ 0_desafios/4_facebook/babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  presets: [
+    "@babel/preset-env", //Responsavel por traduzir as funcionalidades do JS que o browser não entende
+    "@babel/preset-react" //Responsavel por traduzir as funcionalidades do react que o browser não entende
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties'
+  ]
+};
