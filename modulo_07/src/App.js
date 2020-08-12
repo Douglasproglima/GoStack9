89cef901c79004935b46fdf8d5490324bd75087e
Douglas Lima <douglasproglima@gmail.com>
2020-08-11 21:03:59 -0300

    Estilização Global

--- modulo_07/src/App.js
+++ modulo_07/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes />
-      <GlobalStyle></GlobalStyle>
+      <GlobalStyle />
     </BrowserRouter>
   );
 }
--- modulo_07/src/style/global.js
+++ modulo_07/src/style/global.js
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
+import background from '../assets/images/background.svg';
 
 export default createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Roboto:400&display=swap');
   * {
     margin: 0;
     padding: 0;
@@ -9,18 +11,21 @@ export default createGlobalStyle`
   }
 
   html, body, #root  {
+    margin: 0 auto;
+    ${'' /* max-width: 1020px; */}
+    ${'' /* padding: 0 20px 50px; */}
     min-height: 100%;
   }
 
   body {
-    background: #000000d1;
+    background: #191920 url(${background}) no-repeat center top;
     -webkit-font-smoothing: antialiased !important;
   }
 
   body, input, button {
     color: #f47b00;
     font-size: 14px;
-    font-family: Arial, Helvetica, sans-serif;
+    font-family: Roboto, sans-serif;
   }
 
   button {
