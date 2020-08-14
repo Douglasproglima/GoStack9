8dc1e7599448494b13cd4b6cfc43fb1d9b7965e1
Douglas Lima <douglasproglima@gmail.com>
2020-08-13 23:58:17 -0300

    Estilização do Carrinho de Compras

--- modulo_07/src/pages/Cart/index.js
+++ modulo_07/src/pages/Cart/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import {MdRemoveCircleOutline, MdAddCircleOutline, MdDelete} from 'react-icons/md';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import api from '../../services/api';
-import Container from '../../components/Container';
-import { Loading, Owner, IssuesList } from './style';
+//import Container from '../../components/Container';
+import { Container, ProductTable,  Total} from './style';
 
 export default  class Cart extends Component {
 
@@ -12,7 +13,58 @@ export default  class Cart extends Component {
 
     return (
       <Container>
-        <h1>CART</h1>
+        <ProductTable>
+          <thead>
+            <tr>
+              <th></th>
+              <th>PRODUTO</th>
+              <th>QTDE.</th>
+              <th>SUBTOTAL</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <img
+                  src="https://static.netshoes.com.br/produtos/tenis-adidas-vl-court-20-masculino/72/NQQ-0862-172/NQQ-0862-172_zoom2.jpg?ts=1581955412&?ims=544xhttps://static.netshoes.com.br/produtos/tenis-adidas-vl-court-20-masculino/72/NQQ-0862-172/NQQ-0862-172_zoom2.jpg?ts=1581955412&?ims=1088x"
+                  alt="Tênis "
+                />
+              </td>
+              <td>
+                <strong>Tênis Adidas Vl Court 2.0 Masculino - Preto e Cinza</strong>
+                <span>R$ 164,99</span>
+              </td>
+              <td>
+                <div>
+                  <button type="button">
+                    <MdRemoveCircleOutline size={20} color="#f47b00"/>
+                  </button>
+                  <input type="number" readOnly value={2}/>
+                  <button type="button">
+                    <MdAddCircleOutline size={20} color="#f47b00"/>
+                  </button>
+                </div>
+              </td>
+              <td>
+                <strong>R$ 164,99</strong>
+              </td>
+              <td>
+                <button type="button">
+                  <MdDelete size={25} color="#f47b00"/>
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </ProductTable>
+
+        <footer>
+          <button type="button">Finalizar Pedido</button>
+          <Total>
+            <span>TOTAL</span>
+            <strong>R$ 329,00</strong>
+          </Total>
+        </footer>
       </Container>
     );
   };
--- modulo_07/src/pages/Cart/style.js
+++ modulo_07/src/pages/Cart/style.js
@@ -1,4 +1,113 @@
 import styled from 'styled-components';
+import {darken} from 'polished';
+
+export const Container = styled.div`
+  padding: 30px;
+  background: #2929299c;
+  border-radius: 4px;
+  box-shadow: 0 0 25px rgb(113 89 193 / 70%);
+  padding: 30px;
+  margin: 20px auto;
+
+  h1 {
+    font-size: 20px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-bottom: 1px;
+
+    svg {
+      margin-right: 10px;
+    }
+  }
+
+  footer {
+    margin-top: 30px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    button {
+      background: #f47b00;
+      color: #ffffff;
+      border: 0;
+      border-radius: 4px;
+      padding: 12px 20px;
+      font-weight: bold;
+      text-transform: uppercase;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.06, '#f47b00')};
+      }
+    }
+  }
+`;
+
+export const ProductTable = styled.table`
+  width: 100%;
+
+  thead th {
+    color: #ffffff;
+    text-align: left;
+    padding: 12px;
+  }
+
+  tbody td {
+    padding: 12px;
+    border-bottom: 1px solid #f47b00;
+  }
+
+  img {
+    height: 100px;
+  }
+
+  strong {
+    color: #ffffff;
+    display: block;
+  }
+
+  span {
+    display: block;
+    margin-top: 5px;
+    font-size: 18px;
+    font-weight: bold;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+
+    input {
+      border: 1px solid #dddddd;
+      border-radius: 4px;
+      color: #666666;
+      padding: 6px;
+      width: 50px;
+    }
+  }
+
+  button {
+    background: none;
+    border: 0;
+    padding: 6px;
+  }
+`;
+
+export const Total = styled.div`
+  display: flex;
+  align-items: baseline;
+
+  span {
+    color: #ffffff;
+    font-weight: bold;
+  }
+
+  strong {
+    font-size: 28px;
+    margin-left: 5px;
+  }
+`;
 
 export const Loading = styled.div`
   color: #fff;
