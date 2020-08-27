892389fd07e745e6d61c627c703f236ee50dd951
Douglas Lima <douglasproglima@gmail.com>
2020-08-26 21:44:41 -0300

    Correção bug de config do Root Import

--- modulo_09/src/style/global.js
+++ modulo_09/src/style/global.js
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Roboto:400&display=swap');
   * {
     margin: 0;
     padding: 0;
@@ -8,7 +9,12 @@ export default createGlobalStyle`
     box-sizing: border-box;
   }
 
+  *:focus {
+    outline: 0;
+  }
+
   html, body, #root  {
+    height: 100%;
     min-height: 100%;
   }
 
@@ -18,12 +24,20 @@ export default createGlobalStyle`
   }
 
   body, input, button {
-    color: #f47b00;
+    color: #ffffff;
     font-size: 14px;
     font-family: Arial, Helvetica, sans-serif;
   }
 
   button {
     cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
   }
 `;
